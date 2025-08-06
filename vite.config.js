817d452cd88583aba472792ea86b381743c989f5
Subhra007 <berasubhra2004@gmail.com>
2025-08-06 09:38:45 +0530

    Updated 06-08 at 09:38

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    historyApiFallback: true, // ✅ This line tells Vite to fallback to index.html
+  },
 })
